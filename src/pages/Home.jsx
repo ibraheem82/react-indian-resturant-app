b87195d8af41c8d4bb36958412cbe908e640942b
ibraheem82 <ibraheemomikunle82@gmail.com>
2025-04-06 12:37:15 +0100

    ....

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
   return (
     <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
       <div className="headerContainer">
-        <h1 style={{ fontWeight: "bold" }}>Astro🚀👩‍🚀I Corner </h1>
+        <h1 style={{ fontWeight: "bold" }}>🚀👩‍🚀 Corner </h1>
         <p>INDIAN FOOD AT A CLICK</p>
         <Link to="/menu">
           <button> ORDER NOW </button>
